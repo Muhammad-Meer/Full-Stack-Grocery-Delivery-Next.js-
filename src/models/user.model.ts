b4278f516fb6683fe7ee6a0981d43adf93f50838
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-27 03:52:28 -0700

    code

---
+++ src/models/user.model.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  mobile: string;
+  role: "user" | "admin" | "deliveryboy";
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    mobile: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin", "deliveryboy"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
