874b192e0f802e17840e28d7cd2cf15e3faaf8b0
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-28 00:56:17 -0700

    newcodee

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,7 +1,24 @@
-import { NextApiRequest } from "next";
+import connectDB from "@/lib/db";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req:NextApiRequest) {
+export async function POST(req: NextRequest) {
     try {
+        await connectDB()
+        const {name, email, password} = await req.json()
+        const existUser = await User.findOne({email})
+        if(existUser){
+            return NextResponse.json(
+                {message: "email already exist!"},
+                {status:400}
+            )
+        }
+        if(password.lenght <6 ) {
+            return NextResponse.json(
+                {message: "email already exist!"},
+                {status:400}
+            )
+        }
         
     } catch (error) {
         
