b81d532f948bb2e29b00be4adfe96676b64abcd2
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-29 00:14:53 -0700

    newcodee

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,34 @@
+import connectDb from "@/lib/db"
+import User from "@/models/user.model"
+import bcrypt from "bcryptjs"
 import NextAuth from "next-auth"
- 
+import Credentials from "next-auth/providers/credentials"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials:
+      {
+        userneam: { type: "email", label: "userneam", placeholder: "userneam", },
+        password: { type: "password", label: "Password", placeholder: "*****", },
+      },
+      async authorize(credentials, request) {
+        try {
+          await connectDb()
+          const email = await credentials.userneam
+          const password = await credentials.password
+          const user = await User.findOne({email})
+          if(!user) {
+           throw new Error("user does not exist")
+          }
+
+          const isMatch = await bcrypt.compare(password, user.password)
+ 
+        } catch (error) {
+
+        }
+
+      },
+    })
+  ],
 })
